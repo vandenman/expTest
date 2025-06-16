df3a3ffdca5574703a2b2717bdf6cf28b39f98b6
Don van den Bergh <donvdbergh@hotmail.com>
2025-06-16 10:04:46 +0200

    add windows timings

--- bench_exp_and_log.R
+++ bench_exp_and_log.R
@@ -26,6 +26,20 @@ summary(bench_press_exp)[, 1:10]
 # 7 exp            6   4.03ms   4.81ms      216.    7.63MB    75.2     69    24    318.9ms
 # 8 expTest        6   8.71ms   9.44ms      108.    7.63MB    34.1     38    12    352.1ms
 
+# R version 4.5.1 (2025-06-13 ucrt)
+# Platform: x86_64-w64-mingw32/x64
+# Running under: Windows 11 x64 (build 22631)
+# A tibble: 8 × 10
+# expression  size      min   median `itr/sec` mem_alloc `gc/sec` n_itr  n_gc total_time
+# <bch:expr> <int> <bch:tm> <bch:tm>     <dbl> <bch:byt>    <dbl> <int> <dbl>   <bch:tm>
+# 1 exp            3     40µs   41.1µs   22908.     7.86KB     2.29  9999     1      436ms
+# 2 expTest        3   12.1µs   13.6µs   66978.     7.86KB     6.70  9999     1      149ms
+# 3 exp            4  404.2µs  418.6µs    2346.    78.17KB     4.22  1111     2      474ms
+# 4 expTest        4  116.9µs  134.1µs    7286.    78.17KB     8.89  3280     4      450ms
+# 5 exp            5   4.15ms   4.18ms     235.    781.3KB     2.04   115     1      490ms
+# 6 expTest        5   1.27ms   1.36ms     719.    781.3KB    11.3    319     5      444ms
+# 7 exp            6  41.41ms  43.04ms      23.3    7.63MB     8.74     8     3      343ms
+# 8 expTest        6  12.53ms  12.96ms      74.6    7.63MB    12.4     30     5      402ms
 bench_press_log <- bench::press(
   size = 3L:6L,
   {
@@ -53,3 +67,19 @@ summary(bench_press_log)[, 1:10]
 # 6 expTest        5 863.67µs 878.18µs     1057.   781.3KB     12.7   498     6    470.9ms
 # 7 exp            6   5.63ms   5.76ms      168.    7.63MB     42.6    59    15    351.8ms
 # 8 expTest        6   9.29ms   9.51ms      105.    7.63MB     26.9    39    10    371.1ms
+
+
+# R version 4.5.1 (2025-06-13 ucrt)
+# Platform: x86_64-w64-mingw32/x64
+# Running under: Windows 11 x64 (build 22631)
+# # A tibble: 8 × 10
+# expression  size      min   median `itr/sec` mem_alloc `gc/sec` n_itr  n_gc total_time
+# <bch:expr> <int> <bch:tm> <bch:tm>     <dbl> <bch:byt>    <dbl> <int> <dbl>   <bch:tm>
+# 1 exp            3     22µs   23.1µs   41147.     7.86KB     4.12  9999     1      243ms
+# 2 expTest        3   11.2µs   12.6µs   68821.     7.86KB     6.88  9999     1      145ms
+# 3 exp            4  227.2µs  240.5µs    4113.    78.17KB     6.37  1936     3      471ms
+# 4 expTest        4    114µs  124.5µs    7191.    78.17KB     8.72  3298     4      459ms
+# 5 exp            5   2.27ms   2.41ms     408.    781.3KB     6.48   189     3      463ms
+# 6 expTest        5   1.13ms   1.23ms     784.    781.3KB    11.3    348     5      444ms
+# 7 exp            6  23.26ms  23.42ms      42.4    7.63MB    10.6     12     3      283ms
+# 8 expTest        6  11.13ms  11.35ms      87.1    7.63MB    12.8     34     5      390ms
